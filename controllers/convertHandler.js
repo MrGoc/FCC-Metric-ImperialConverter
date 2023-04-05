66ac18fcee9e41d72fa0a2102027aa266a0e5427
MrGoc <mrgoc@live.com>
2023-04-05 14:25:08 +0200

    feat: Implement getNum in convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,63 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+  this.getNum = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+    let num = input;
+    //let index = /[a-z]/i.exec(input).index;
+    let char = num.match("[a-zA-Z]");
+    let index = -1;
+    if (char !== null) index = str.indexOf(char);
+
+    if ((index = -1)) num = input;
+    else num = input.substring(0, index);
+
+    if (num.includes("./")) result = "invalid number";
+    else if (num.includes("/")) {
+      let nums = num.split("/");
+      if (nums.length !== 2) result = "invalid number";
+      else {
+        if (isNaN(nums[0]) || isNaN(nums[1])) result = "invalid number";
+        else result = +nums[0] / +nums[1];
+      }
+    } else {
+      if (isNaN(num)) result = "invalid number";
+      else result = +num;
+    }
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getUnit = function (input) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let result;
+
+    return result;
+  };
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const expect = require("chai").expect;
 const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  app.route("/api/convert").post((req, res) => {
+  app.route("/api/convert").get((req, res) => {
     let convertHandler = new ConvertHandler();
     let initUnit = convertHandler.getUnit(req.query.input);
     let initNum = convertHandler.getNum(req.query.input);
